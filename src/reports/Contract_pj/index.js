265af5fd0e60a2702d60ad89d8433e515e74a81f
aadionisio <aadionisio@gmail.com>
2020-06-18 17:25:45 -0300

    Fim da primeira folha

--- src/reports/Contract_pj/index.js
+++ src/reports/Contract_pj/index.js
@@ -24,6 +24,78 @@ function Contract_pj() {
         4.886/65 e 8.420/92.
       </ContentTopic>
       <Topic>CLÁUSULA PRIMEIRA – DO OBJETO CONTRATUAL</Topic>
+      <ContentTopic>
+        O Objeto contratual é a intermediação de negócios mercantis, em
+        conformidade com art. 1º da Lei 4.886/65, visando a venda de produtos do
+        ramo comercial da REPRESENTADA, que é o{" "}
+        <strong>
+          <i>
+            <u>
+              atacado de secos e molhados, compreendendo gêneros alimentícios,
+              latarias, limpeza doméstica, perfumarias, medicamentos, miudezas,
+              etc
+            </u>
+          </i>
+        </strong>
+        ., utilizando-se de listas de preços fornecidas previamente pela
+        REPRESENTADA e em consonância com as condições de comercialização
+        previamente estabelecidas.
+      </ContentTopic>
+      <ContentTopic>
+        <strong>Parágrafo Único</strong> - A REPRESENTADA poderá, a qualquer
+        tempo, alterar suas listas de preços, diretivas e condições de
+        comercialização, independentemente de concordância do REPRESENTANTE,
+        revogando, destarte, a anterior no que for incompatível.
+      </ContentTopic>
+      <Topic>CLÁUSULA SEGUNDA – DO PRAZO</Topic>
+      <ContentTopic>
+        O presente Contrato de Representação Comercial é firmado,{" "}
+        <strong>
+          <u>
+            <i>sem vínculo empregatício</i>
+          </u>
+        </strong>{" "}
+        e na forma estabelecida pelas Leis Federais nº 4.886, de 09 de dezembro
+        de 1.965, e 8.420 de 08 de maio de 1992, por prazo indeterminado,
+        conforme faculta o <strong>art. 27, alínea “c” </strong> , tendo seu
+        início na data da assinatura deste Instrumento.
+      </ContentTopic>
+      <Topic>CLÁUSULA TERCEIRA – DA ZONA DE ATUAÇÃO</Topic>
+      <ContentTopic>
+        O REPRESENTANTE prestará os serviços aqui pactuados, sem caráter de
+        exclusividade, nas praças de Agua Branca, Amarante, Angical do Piauí,
+        Jardim do Mulato, Oeiras, Palmeiras e Regeneração à sua conveniência,
+        designar outros Contratados para atuarem nesta mesma zona de atuação, em
+        consonância com o{" "}
+        <strong>art. 27, alíneas “d” e “e”, da Lei 4.886/65</strong>.
+      </ContentTopic>
+      <ContentTopic>
+        <strong>Parágrafo Único</strong> - O REPRESENTANTE{" "}
+        <strong>
+          <u>
+            <i>não terá exclusividade em sua zona de atuação</i>
+          </u>
+        </strong>{" "}
+        e, por conseguinte, não fará jus à comissão pelos negócios ali
+        realizados diretamente pela REPRESENTADA ou por outros Contratados,
+        conforme estabelecido no{" "}
+        <strong>art. 31 e seu parágrafo único, da Lei 4.886/65</strong>.
+      </ContentTopic>
+      <Topic>CLÁUSULA QUARTA – DAS COMISSÕES DO REPRESENTANTE</Topic>
+      <ContentTopic>
+        A REPRESENTANTE terá direito a uma comissão em conformidade com o{" "}
+        <strong>art. 32, parágrafo 4º, da Lei 4.886/65</strong>, que variará de
+        produto para produto, em torno{" "}
+        <strong>
+          <i>
+            <u>
+              de 1% (um por cento) à 3% (três por cento), não ultrapassando a 3%
+              (três por cento)
+            </u>
+          </i>
+        </strong>
+        .
+      </ContentTopic>
     </Container>
   );
 }
